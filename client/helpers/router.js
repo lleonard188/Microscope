a6815658fc50866f899c45df81ae0d26a3658d24
Len Stokes <lleonard188@gmail.com>
2013-12-29 22:37:40 -0500

    Added restrictions to submitting posts, etc.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -4,5 +4,20 @@ Meteor.Router.add({
 	'/posts/:_id': {
 		to: 'postPage',
 		and: function(id) { Session.set('currentPostId', id); } 
-	}
+	},
+
+	'/submit' : 'postSubmit'
 });	
+
+Meteor.Router.filters({
+	'requireLogin': function(page) {
+		if (Meteor.user()) 
+			return page;
+		else if (Meteor.loggingIn())
+			return 'loading';
+		else
+			return 'accessDenied';
+	}
+})
+
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,21 @@
+Template.postSubmit.events({
+	'submit form': function(event) {
+		event.preventDefault();
+
+		var post = {
+			url: $(event.target).find('[name=url]').val(),
+			title: $(event.target).find('[name=title]').val(),			
+			message: $(event.target).find('[name=message]').val()
+		}
+
+		post._id = Posts.insert(post);
+		Meteor.Router.to('postPage', post);
+
+		Meteor.call('post', post, function(error,id) {
+			if (error)
+				return alert(error.reason);
+
+			Meteor.Router.to('postPage', id);
+		});
+	}
+});	
